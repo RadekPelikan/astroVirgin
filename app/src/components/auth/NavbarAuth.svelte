a66b4d63d45bcffd345442a004ea190eee04b072
Radek Pelikan <pelikan.radek03@gmail.com>
2025-12-04 01:29:14 +0100

    profile page

--- app/src/components/auth/NavbarAuth.svelte
+++ app/src/components/auth/NavbarAuth.svelte
@@ -1,16 +1,9 @@
 <script lang="ts">
 	import PocketBase from 'pocketbase';
 	import { authLinks } from '../../config/navbar';
-	import { onMount } from 'svelte';
-	const pb = new PocketBase('http://127.0.0.1:8090');
+	import type { TypedPocketBase } from '../../types/pocketbase-types.generated';
 
-	let isValid = $state(false)
-
-
-	onMount(() => {
-	console.log("mount");
-		isValid = pb.authStore.isValid;
-	})
+	const pb = new PocketBase('http://127.0.0.1:8090') as TypedPocketBase;
 
 	const onLogoutHandler = (e: MouseEvent) => {
 		pb.authStore.clear();
@@ -19,11 +12,11 @@
 </script>
 
 <ul class="grid gap-2 sm:flex sm:h-full">
-	{#if isValid}
+	{#if pb.authStore.isValid}
 		<li class="grid justify-center">
-			<a href="" class="">
+			<a href={`/profile/${pb.authStore.record?.id}`} class="">
 				<img
-					src={`http://127.0.0.1:8090/api/files/users/${pb.authStore?.record.id}/${pb.authStore?.record.avatar}`}
+					src={`http://127.0.0.1:8090/api/files/users/${pb?.authStore?.record.id}/${pb.authStore?.record.avatar}`}
 					class="aspect-square w-10 rounded-full"
 					alt=""
 				/>
